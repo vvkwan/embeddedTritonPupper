75cff3ce6f62151843c9db1bda476412f594cfd8
vvkwan <94148836+vvkwan@users.noreply.github.com>
2025-11-08 14:39:44 -0800

    scripts

---
+++ scripts/install_ros2.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+apt-get update -y
+apt-get install -y ros-humble-ros-base rose-dev-tools
+echo 'source /opt/ros/humble/setup.bash' >> /root/.bashrc
+echo 'source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash' >> /root/.bashrc || true
